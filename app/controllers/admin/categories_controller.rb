ba56361b01e0bdec127269a8c71565cf1b1421ee
HongJun Jin <hongjunjin@HongJuns-MacBook-Pro.local>
2013-11-13 19:03:09 -0800

    final

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,8 +25,12 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.find(params[:id])
+    @category = Category.new
+    if (params[:id] != nil)
+        @category = Category.find(params[:id])
+    end
     @category.attributes = params[:category]
+    
     if request.post?
       respond_to do |format|
         format.html { save_category }
